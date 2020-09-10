2b9dcb564911859d58fed26605004fda80dac51e
Sean Baier <sean.t.baier@gmail.com>
2020-09-10 08:07:34 -0500

    added tests

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 from app.api.api_v1.api import router as api_router
-from mangum import Mangum
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message`": "Hello World!"}
+    return {"message": "Hello World!"}
 
 
 app.include_router(api_router, prefix="/api/v1")
