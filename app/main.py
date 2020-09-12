c059352a107cd73070055415f8cbad59b23ec331
Sean Baier <sean.t.baier@gmail.com>
2020-09-12 14:58:39 -0500

    adding envs

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-
-from app.api.api_v1.api import router as api_router
 from mangum import Mangum
+from app.core import config
+from app.api.api_v1.api import router as api_router
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "CICD Successful!"}
+    return {"message": f"This is our secret key: {config.settings.secret_key}"}
 
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix=config.settings.prefix)
 handler = Mangum(app)
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "CICD Successful!"}
+    assert response.json() == {"message": "This is our secret key: Secret Value"}
